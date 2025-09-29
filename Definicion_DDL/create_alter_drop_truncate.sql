9e98de94a8c691f5d1815d20166ab5333a5988af
JATeR912 <jater.code@gmail.com>
2025-09-28 23:44:21 -0300

    Actualizacion archivo DDL

--- Definicion_DDL/create_alter_drop_truncate.sql
+++ Definicion_DDL/create_alter_drop_truncate.sql
@@ -12,3 +12,26 @@ CREATE TABLE IF NOT EXISTS categorias(
   orden_categoria INT NOT NULL
 );
 
+-- Insertar datos de prueba en tabla categorias
+INSERT INTO categorias (nombre_categoria, descripcion_categoria, orden_categoria) VALUES
+('Desarrollo Web', 'Desarrollo de sitios y aplicaciones web.', 10);
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- Modificar tabla proyectos para agregar columna id_categoria
+ALTER TABLE proyectos
+  ADD COLUMN id_categoria INT AFTER id_modulo,
+  ADD FOREIGN KEY (id_categoria) REFERENCES categorias(id_categorias);
+
+-- Eliminar columna id_modulo de tabla proyectos
+ALTER TABLE proyectos
+  DROP COLUMN id_modulo;
+
+-- Truncar tabla categorias
+TRUNCATE TABLE modulos;
+
+-- Eliminar tabla modulos si ya no es necesaria
+DROP TABLE IF EXISTS modulos;
+
+SET FOREIGN_KEY_CHECKS = 1;
+-- SET FOREIGN_KEY_CHECKS -- Desactivar temporalmente y activar luego las comprobaciones de claves foráneas
